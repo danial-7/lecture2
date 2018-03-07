d9aaa78e398d8eacb8389773440fd80f77babf77
danial-7 <36899363+danial-7@users.noreply.github.com>
2018-03-07 09:11:33 +0330

    Update 6_1_if_statement.py

--- 6_1_if_statement.py
+++ 6_1_if_statement.py
@@ -4,10 +4,10 @@
 a=5
 b=6
 if(a == b):
-    print("a == b")
+    print("equals")
 if(a > b):
-    print("a > b")
+    print("a is more than b")
 elif(a<b):
-    print("a < b")
+    print("b is more than b")
 else:
     pass # do nothing
